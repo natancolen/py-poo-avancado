ba08833d840ce65f11650ec891fb9f14ff5f27fc
natancolen <natancolen@gmail.com>
2023-04-09 11:39:24 -0300

    Adicionando atributos e metodos

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 class Filme():
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
 
 class Serie():
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
 
 if __name__ == '__main__':
-    vingadores = Filme('Vingadores - Guerra Infinita', 2018, 149)
-    monstro_pantano = Serie('Monstro do pantano', 2019, 1)
+    vingadores = Filme('vingadores - guerra infinita', 2018, 149)
+    monstro_pantano = Serie('monstro do pantano', 2019, 1)
 
-    print(f'Filme: {vingadores.nome} do ano {vingadores.ano} com o tempo de duraçao {vingadores.duracao} min ')
-    print(f'Serie: {monstro_pantano.nome} do ano {monstro_pantano.ano} com o total de {monstro_pantano.temporadas} temporadas')
+    vingadores.dar_like()
+    monstro_pantano.dar_like()
+    monstro_pantano.dar_like()
+
+    print(f'Filme: {vingadores.nome} - Ano: {vingadores.ano} - Duraçao: {vingadores.duracao} min - Likes: {vingadores.likes}')
+    print(f'Serie: {monstro_pantano.nome} - Ano: {monstro_pantano.ano} - Temporadas: {monstro_pantano.temporadas} - Likes: {monstro_pantano.likes}')
 
 
