2c1e9a8a99861b70024b63a3b2c0652d01051228
natancolen <natancolen@gmail.com>
2023-04-09 11:24:51 -0300

    Criando classes Filme e serie

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+class Filme():
+    def __init__(self, nome, ano, duracao):
+        self.nome = nome
+        self.ano = ano
+        self.duracao = duracao
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Serie():
+    def __init__(self, nome, ano, temporadas):
+        self.nome = nome
+        self.ano = ano
+        self.temporadas = temporadas
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    vingadores = Filme('Vingadores - Guerra Infinita', 2018, 149)
+    monstro_pantano = Serie('Monstro do pantano', 2019, 1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(f'Filme: {vingadores.nome} do ano {vingadores.ano} com o tempo de duraçao {vingadores.duracao} min ')
+    print(f'Serie: {monstro_pantano.nome} do ano {monstro_pantano.ano} com o total de {monstro_pantano.temporadas} temporadas')
+
+
