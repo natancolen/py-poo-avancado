3ac246d06c9c5f70807c8be1a00a8021603ade3c
natancolen <natancolen@gmail.com>
2023-04-12 23:00:31 -0300

    reutilizando ainda mais

--- main.py
+++ main.py
@@ -21,47 +21,13 @@ class Programa():
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self._likes = 0
-
-    @property
-    def likes(self):
-        return self._likes
-
-    @property
-    def nome(self):
-        return self._nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self._nome = novo_nome.title()
-
-    def dar_like(self):
-        self._likes += 1
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self._likes = 0
-
-    @property
-    def likes(self):
-        return self._likes
-
-    @property
-    def nome(self):
-        return self._nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self._nome = novo_nome.title()
-
-    def dar_like(self):
-        self._likes += 1
 
 if __name__ == '__main__':
     vingadores = Filme('vingadores - guerra infinita', 2018, 149)
