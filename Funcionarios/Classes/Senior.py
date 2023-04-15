021baebb20db35460a4a02636de87724c735cdaa
natancolen <natancolen@gmail.com>
2023-04-15 12:20:16 -0300

    Mixins

--- Funcionarios/Classes/Senior.py
+++ Funcionarios/Classes/Senior.py
@@ -1,5 +1,6 @@
 from Funcionarios.Classes.Alura import Alura
 from Funcionarios.Classes.Caelum import Caelum
+from Funcionarios.Classes.Hipster import Hipster
 
-class Senior(Alura, Caelum):
+class Senior(Alura, Caelum, Hipster):
     pass
--- Funcionarios/Classes/Setup.py
+++ Funcionarios/Classes/Setup.py
@@ -4,13 +4,5 @@ from Funcionarios.Classes.Senior import Senior
 
 class Pessoa:
     def exibir(self):
-        jose = Junior()
-        jose.busca_perguntas_sem_resposta()
-        jose.mostrar_tarefas()
-
-        print('---')
-
-        luan = Pleno()
-        luan.busca_perguntas_sem_resposta()
-        luan.busca_cursos_do_mes()
-        luan.mostrar_tarefas()
+        luan = Senior('Luan')
+        print(luan)
